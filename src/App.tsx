802cbcd6ee283751977bbbfbcb8cfb01d519fe92
Robin van den Berg <robin.vandenberg@punchcard.io>
2020-07-30 15:04:59 -0600

    Add validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Year from './year';
-
+import classnames from 'classnames';
 interface IProps {
 }
 
@@ -10,12 +10,11 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 
 	const [values2019, setValues2019] = React.useState<Array<any>>(defaultValues2019);
 	const [values2020, setValues2020] = React.useState<Array<any>>(defaultValues2020);
-	
-	let year = new Year(values2019, values2020, [], []);
-	year.getvalues();
-	let results = year.finalGeneralResults;
-	let altResults = year.finalAltResults;
-	
+	const [resultsGeneral, setResultsGeneral] = React.useState<number[]>([]);
+	const [resultsAlt, setResultsAlt] = React.useState<number[]>([]);
+	const [fieldsWithErrors, setfieldsWithErrors] = React.useState<string[]>([])
+
+	React.useEffect(init, []);
 	return (
 		<div className="container">
 			<nav className="navbar navbar-expand-lg navbar-primary">
@@ -30,40 +29,83 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 				</div>
 				<div className="col-lg-9">
 					<div className="row">
+						<div className="col-lg-2">
+							&nbsp;
+						</div>
 						<div className="col-lg-3">
 							2019
 						</div>
 						<div className="col-lg-3">
 							2020
 						</div>
-						<div className="col-lg-3">
+						<div className="col-lg-2">
 							General
 						</div>
-						<div className="col-lg-3">
-							Alternative</div>
+						<div className="col-lg-2">
+							Alt.
+						</div>
 					</div>
 					{values2019.map((item, index: number) => {
 						return (
 							<div className="row">
+								<div className="col-lg-2"></div>
 								<div className="col-lg-3">
-									<input
-										className="form-control"
-										value={item}
-										onChange={(val) => updateArray2019(index, val)}
-									/>
+									<div className="input-group mb-3">
+										<div className="input-group-prepend">
+											<span className="input-group-text">$</span>
+										</div>
+										<input
+											className={classnames('form-control', fieldsWithErrors.includes(`2019${index}`) && "is-invalid")}
+											value={item}
+											onChange={(evt) => {
+												updateArray2019(index, evt);
+												if( !isNaN(evt.target.value as any) ) {
+													if(fieldsWithErrors.includes(`2019${index}`)) {
+														let i = fieldsWithErrors.findIndex((v) => v === `2019${index}`);
+														let oldValues = [...fieldsWithErrors];
+														oldValues.splice(i, 1);
+														setfieldsWithErrors(oldValues);
+													}
+													update();
+												} else {
+													setfieldsWithErrors([...fieldsWithErrors, `2019${index}`]);
+													clear();
+												}
+											}}
+										/>
+									</div>
 								</div>
 								<div className="col-lg-3">
-									<input
-										className="form-control"
-										value={values2020[index]}
-										onChange={(val) => updateArray2020(index, val)}
-									/>
+									<div className="input-group mb-3">
+										<div className="input-group-prepend">
+											<span className="input-group-text">$</span>
+										</div>
+										<input
+											className={classnames('form-control', fieldsWithErrors.includes(`2020${index}`) && "is-invalid")}
+											value={values2020[index]}
+											onChange={(evt) => {
+												updateArray2020(index, evt);
+												if( !isNaN(evt.target.value as any) ) {
+													if(fieldsWithErrors.includes(`2020${index}`)) {
+														const i = fieldsWithErrors.findIndex((v) => v === `2020${index}`);
+														const oldValues = [...fieldsWithErrors];
+														oldValues.splice(i, 1);
+														setfieldsWithErrors(oldValues);
+													}
+													update();
+												} else {
+													setfieldsWithErrors([...fieldsWithErrors, `2020${index}`]);
+													clear();
+												}
+											}}
+										/>
+									</div>	
 								</div>
-								<div className="col-lg-3">
-									{Math.round(results[index] * 100)}%
+								<div className="col-lg-2">
+									{resultsGeneral[index] !== undefined ? ( `${Math.round(resultsGeneral[index] * 100)}%`) : ""}
 								</div>
-								<div className="col-lg-3">
-									{Math.round(altResults[index] * 100)}%
+								<div className="col-lg-2">
+									{resultsAlt[index] !== undefined ? ( `${Math.round(resultsAlt[index] * 100)}%`) : ""}
 								</div>
 							</div>
 						)
@@ -86,7 +128,26 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 		oldValues[arrindex] = value;
 		setValues2020(oldValues);
 	}
-
+	function init() {
+		const year = new Year(defaultValues2019, defaultValues2020, [], []);
+		year.getvalues();
+		setResultsGeneral(year.finalGeneralResults);
+		setResultsAlt(year.finalAltResults);
+	}
+	function update() {
+		if ( fieldsWithErrors.length === 0 ) {
+			const year = new Year(values2019, values2020, [], []);
+			year.getvalues();
+			setResultsGeneral(year.finalGeneralResults);
+			setResultsAlt(year.finalAltResults);
+		} else {
+			clear();
+		}
+	}
+	function clear() {
+		setResultsGeneral([]);
+		setResultsAlt([]);
+	}
 };
 
 export default App;
