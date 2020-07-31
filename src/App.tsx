fce2a02ddb646cc1e831fff334736fd8acc146d4
Estyn Edwards <estyn@punchcard.io>
2020-07-30 22:34:16 -0600

    testing build

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import Year from './year';
 interface IProps {
 }
 
-const App: React.FC<IProps> = ( props: IProps ) => {
+const App: React.FC<IProps> = (props: IProps) => {
 	const defaultValues2019 = [83000, 190000, 140000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 140000];
 	const defaultValues2020 = [85000, 85000, 140000, 65000, 100000, 60000, 8000, 85000, 8000, 0, 0, 0];
 
 	const [values2019, setValues2019] = React.useState<Array<any>>(defaultValues2019);
 	const [values2020, setValues2020] = React.useState<Array<any>>(defaultValues2020);
-	
+
 	let year = new Year(values2019, values2020, [], []);
 	year.getvalues();
 	let results = year.finalGeneralResults;
 	let altResults = year.finalAltResults;
-	
+
 	return (
 		<div className="container">
 			<nav className="navbar navbar-expand-lg navbar-primary">
@@ -24,7 +24,7 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 			<div className="row">
 				<div className="col-lg-3">
 					<div className="alert alert-warning">
-						Canada Emergency Wage Subsidy Estimator (Periods 1-9)
+						Canada Emergency Wage Subsidy Estimator (Periods 1-9)!
 						This estimator reflects the authors’ original understanding of the Canada Emergency Wage Subsidy Rules.  Note that there are many exceptions and variables associated with the rules.  Further, as this program is new, there is limited guidance and court/government interpretation.  Due to the high level of uncertainties, variables, and potential for error, the results of the estimator cannot be relied upon for the filing of a claim, but rather, is meant to be used for estimation purposes.  Professional assistance from your accountant should be sought.  No party associated with the creation, presentation, or distribution of the estimator may be held liable in any way.  Further details on the terms of use can be found here (link this to a disclaimer).
 					</div>
 				</div>
@@ -67,7 +67,7 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 								</div>
 							</div>
 						)
-					})}					
+					})}
 				</div>
 			</div>
 		</div>
