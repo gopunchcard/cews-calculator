7cab6e4df31cea80d0096d0e75b4d5a01d33f955
Darcy Chan <darcy@punchcard.io>
2020-07-30 21:34:08 -0600

    Adjusted visual hierarchy of the form

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Year from './year';
 import classnames from 'classnames';
 import NumberFormat from 'react-number-format';
-import { Modal } from './components';
 
 interface IProps {
 }
@@ -18,55 +17,67 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 	const [resultsGeneral, setResultsGeneral] = React.useState<number[]>([]);
 	const [resultsAlt, setResultsAlt] = React.useState<number[]>([]);
 	const [fieldsWithErrors, setfieldsWithErrors] = React.useState<string[]>([]);
-	const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false)
 
 	React.useEffect(init, []);
 	React.useEffect(update, [values2019, values2020]);
 	return (
 		<div className="container py-4">
-			<h1 className="h4 mb-3">
-				Canadian Wage Subsidy Calculator
-			</h1>
 			<div className="row">
 				<div className="col-lg-4">
-					<div className="alert bg-light small">
-						<h2 className="h6">Canada Emergency Wage Subsidy Estimator (Periods 1-9)</h2>
+					<div className="alert bg-light">
+						<h1 className="h2">
+							Canada Emergency Wage Subsidy Estimator <br />
+							<small>(Periods 1-9)</small>
+						</h1>
 						<p>This estimator reflects the authors’ original understanding of the Canada Emergency Wage Subsidy Rules.  Note that there are many exceptions and variables associated with the rules.  Further, as this program is new, there is limited guidance and court/government interpretation.  Due to the high level of uncertainties, variables, and potential for error, the results of the estimator cannot be relied upon for the filing of a claim, but rather, is meant to be used for estimation purposes.  Professional assistance from your accountant should be sought.  No party associated with the creation, presentation, or distribution of the estimator may be held liable in any way.  Further details on the terms of use can be found here (link this to a disclaimer).</p>
 					</div>
-					<button onClick={() => setIsModalOpen(true)}>Open test modal</button>
 				</div>
 				<div className="col-lg-8">
-					<div className="row">
-						<div className="col-2">
+					<div className="row text-uppercase text-monospace text-right">
+						<div className="col-lg-7 offset-lg-1 font-weight-bold text-center border-right border-left">
+							Enter your revenue below
+						</div>
+						<div className="col-lg-4 font-weight-bold">
+							Amount covered
+						</div>
+					</div>
+					<div className="row text-uppercase text-monospace text-right">
+						<div className="col-lg-1 small">
+							Per.
+						</div>
+						<div className="col-lg-1 border-left">
 							&nbsp;
 						</div>
-						<div className="col-3 text-uppercase text-monospace small">
+						<div className="col-lg-3 small text-left">
 							2019
 						</div>
-						<div className="col-3 text-uppercase text-monospace small">
+						<div className="col-lg-3 small text-left border-right">
 							2020
 						</div>
-						<div className="col-2 text-uppercase text-monospace small">
+						<div className="col-lg-2 small">
 							General
 						</div>
-						<div className="col-2 text-uppercase text-monospace small">
+						<div className="col-lg-2 small">
 							Alt.
 						</div>
 					</div>
 					{values2019.map((item, index: number) => {
 						return (
-							<div className={`row ${index % 2 && 'bg-light'}`}>
-								<div className="col-2 text-uppercase text-monospace small text-right">
-									{	index - periodStart >= 0 && index - periodStart + 1 } { monthLabels[index] }
+							<div className={classnames('row', index % 2 && 'bg-light')}>
+								<div className="col-lg-1 text-uppercase text-monospace small text-right align-self-center">
+									{	index - periodStart >= 0 ? index - periodStart + 1 : '-'}
 								</div>
-								<div className="col-3">
-									<div className="input-group mb-3">
+								<div className="col-lg-1 text-uppercase text-monospace small border-left d-flex align-items-center justify-content-end">
+									{ monthLabels[index] }
+								</div>
+								<div className="col-lg-3 py-1">
+									<div className="input-group">
 										<div className="input-group-prepend">
 											<span className="input-group-text">$</span>
 										</div>
 										<NumberFormat
 											thousandSeparator=","
-											className={classnames('form-control', fieldsWithErrors.includes(`field-2019-${index}`) && "is-invalid")}
+											className={classnames('form-control text-right', fieldsWithErrors.includes(`field-2019-${index}`) && "is-invalid")}
 											value={values2019[index]}
 											onValueChange={({floatValue}) => {
 												updateArray2019(index, floatValue);
@@ -79,14 +90,14 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 										/>
 									</div>
 								</div>
-								<div className="col-3">
-									<div className="input-group mb-3">
+								<div className="col-lg-3 border-right py-1">
+									<div className="input-group">
 										<div className="input-group-prepend">
 											<span className="input-group-text">$</span>
 										</div>
 										<NumberFormat
 											thousandSeparator=","
-											className={classnames('form-control', fieldsWithErrors.includes(`field-2020-${index}`) && "is-invalid")}
+											className={classnames('form-control text-right', fieldsWithErrors.includes(`field-2020-${index}`) && "is-invalid")}
 											value={values2020[index]}
 											onValueChange={({floatValue}) => {
 												updateArray2020(index, floatValue);
@@ -99,20 +110,25 @@ const App: React.FC<IProps> = ( props: IProps ) => {
 										/>
 									</div>	
 								</div>
-								<div className="col-2">
-									{resultsGeneral[index] !== undefined ? ( `${Math.round(resultsGeneral[index] * 100)}%`) : ""}
+								<div className="col-lg-2 d-flex align-items-center justify-content-end">
+									{resultsGeneral[index] !== undefined ? ( 
+										<span className={classnames(resultsGeneral[index] > 0 && 'font-weight-bold')}>{Math.round(resultsGeneral[index] * 100)}%</span>)
+									: 
+										"-"
+									}
 								</div>
-								<div className="col-2">
-									{resultsAlt[index] !== undefined ? ( `${Math.round(resultsAlt[index] * 100)}%`) : ""}
+								<div className="col-lg-2 d-flex align-items-center justify-content-end">
+									{resultsAlt[index] !== undefined ? (
+										<span className={classnames(resultsAlt[index] > 0 && 'font-weight-bold')}>{Math.round(resultsAlt[index] * 100)}%</span>)
+									:
+										"-"
+									}
 								</div>
 							</div>
 						)
 					})}					
 				</div>
 			</div>
-			<Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} shouldCloseOnEsc={true}>
-				hello
-			</Modal>
 		</div>
 	);
 
