3f1d77327165e62c920cbca57b96fb23946dd9d1
Estyn Edwards <estyn@punchcard.io>
2020-08-04 17:16:10 -0600

    fix div/0

--- src/calcPercentLossNoNegative.ts
+++ src/calcPercentLossNoNegative.ts
@@ -1,10 +1,8 @@
 
 function calcPercentLostNoNegative(oldValue: number, newValue: number) {
-    if (newValue === 0)
-        return 0.1
-    if (oldValue === 0)
-        oldValue = 0.1
-    return ((-(newValue - oldValue) / oldValue) > .999999 || (-(newValue - oldValue) / oldValue) <= 0) ? 0 : (-(newValue - oldValue) / oldValue);
+    if (oldValue === 0 || newValue === 0)
+        return 0;
+    return ((-(newValue - oldValue) / oldValue) > 1 || (-(newValue - oldValue) / oldValue) <= 0) ? 0 : (-(newValue - oldValue) / oldValue);
 }
 
 export default calcPercentLostNoNegative;
