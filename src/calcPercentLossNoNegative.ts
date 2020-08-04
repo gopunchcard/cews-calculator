fb3eae8ad3ef030a7d26c959772d8996d062ef03
Estyn Edwards <estyn@punchcard.io>
2020-08-04 12:00:33 -0600

    Treat 0 as 1

--- src/calcPercentLossNoNegative.ts
+++ src/calcPercentLossNoNegative.ts
@@ -1,6 +1,9 @@
 
 function calcPercentLostNoNegative(oldValue: number, newValue: number) {
-    if (newValue === 0 || oldValue === 0) return 0
+    if (newValue === 0)
+        return 0.1
+    if (oldValue === 0)
+        oldValue = 0.1
     return ((-(newValue - oldValue) / oldValue) > .999999 || (-(newValue - oldValue) / oldValue) <= 0) ? 0 : (-(newValue - oldValue) / oldValue);
 }
 
