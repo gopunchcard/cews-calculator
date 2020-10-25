b33bae21168d63c0557ca2dc629a2fa2b8a28415
Estyn Edwards <estyn@punchcard.io>
2020-10-25 13:47:47 -0600

    Text corrections from Joe

--- src/content/GeneralNotes.tsx
+++ src/content/GeneralNotes.tsx
@@ -10,7 +10,7 @@ const GeneralNotes: React.FC<IProps> = (_props: IProps) => {
 				The following are the general rules that the Estimator attempts to follow<sup>12</sup>.  Note that there are many exceptions.  Further, clarification from CRA as to how many aspects of the rules work has not yet been received.  These assumptions and calculations are based on our understanding of the rules but cannot be relied upon for the filing of a claim.  Professional assistance is required. For the government application site, click <a href="https://www.canada.ca/en/revenue-agency/services/subsidy/emergency-wage-subsidy.html">here</a>.
 			</p>
 			<p>
-				The calculation can be separated into four period groups: Periods 1-4 (March 15 to July 4), Periods 5-6 (July 5 to August 29), Periods 7 – 9 (August 30 to November 21), and Period 10 (November 22 to December 19).
+				The calculation can be separated into three period groups: Periods 1-4 (March 15 to July 4), Periods 5-6 (July 5 to August 29), and Periods 7 – 10 (August 30 to December 19).
 
 			</p>
 			<h3>Periods 1-4 (original method)</h3>
@@ -40,7 +40,7 @@ const GeneralNotes: React.FC<IProps> = (_props: IProps) => {
 			<p>
 				The greater of the results as calculated under the Period 1-4 method (original) and the Period 7-9 method (new) will be used.  Note that an election to use the average of January and February 2020 as the prior reference period<sup>4</sup> in respect of periods 5-9, means that the computation under both the old and new methods for Period 5 must incorporate that election, regardless of whether the election was made for periods 1-4.
 			</p>
-			<h3>Periods 7-9</h3>
+			<h3>Periods 7-10</h3>
 			<p>
 				The subsidy is split into two components: the base subsidy and the top-up subsidy.  The subsidy is the sum of the two components.
 			</p>
@@ -49,24 +49,24 @@ const GeneralNotes: React.FC<IProps> = (_props: IProps) => {
 				<strong>General –</strong> Gross revenue<sup>1</sup> from the current reference period<sup>2</sup> is compared to that of the   same month in 2019 (prior reference period<sup>4</sup>).
 			</p>
 			<p>
-				<strong>Election –</strong> An election can be made to use the average monthly gross revenues from January and February of 2020 as the prior reference period4 gross revenue. We refer to this as the “Alternative Calculation”<sup>6</sup>.  If the election is made, the alternative calculation method must be used for all of these periods (5-9).
+				<strong>Election –</strong> An election can be made to use the average monthly gross revenues from January and February of 2020 as the prior reference period4 gross revenue. We refer to this as the “Alternative Calculation”<sup>6</sup>.  If the election is made, the alternative calculation method must be used for all of these periods (5-10).
 			</p>
 			<p>
-				<strong>Safe Harbour –</strong> – If the revenue reduction percentage<sup>5</sup> is greater for the immediately preceding period, that prior period decline will be deemed to be the current revenue reduction percentage<sup>5</sup>.  However, if the prior period also benefited from the deemed decline, the actual decline from the prior period must be used to determine if there is a deemed decline in the current period.
+				<strong>Safe Harbour –</strong> If the revenue reduction percentage<sup>5</sup> is greater for the immediately preceding period, that prior period decline will be deemed to be the current revenue reduction percentage<sup>5</sup>.  However, if the prior period also benefited from the deemed decline, the actual decline from the prior period must be used to determine if there is a deemed decline in the current period.
 			</p>
 
 			<p>
-				<strong>Base Subsidy Calculation –</strong> Once the revenue reduction percentage<sup>5</sup> is determined based on the above, it is multiplied by a pre-defined factor which decreases over time.  The factors and related claim caps are: 1.2 and 60% for Periods 5 and 6, 1.0 and 50% for Period 7, and 0.8 and 40% for Period 8, Period 9 was previously set at 0.4 and 20%, however, periods 9 and 10 were subsequently announced to be frozen at the maximum 65% claim from period 8. This percentage would generally be applied to the amount of remuneration paid to the employee for the eligibility period, on remuneration of up to $1,129 per week.  For non-arm’s length employees, the percentage would be applied to the employee’s weekly eligible remuneration<sup>8</sup> or pre-crisis remuneration<sup>9</sup>, whichever is less, up to a maximum of $1,129.  These limits on eligible remuneration apply to Periods 5 through 9.
+				<strong>Calculation –</strong> Once the revenue reduction percentage<sup>5</sup> is determined based on the above, it is multiplied by a pre-defined factor which decreases over time.  The factors and related claim caps are: 1.2 and 60% for Periods 5 and 6, 1.0 and 50% for Period 7, and 0.8 and 40% for Periods 8, 9 and 10. Perriod 9 was previously set at 0.4 and 20%, however, periods 9 and 10 were subsequently announced to be frozen at the maximum 65% claim from period 8. This percentage would generally be applied to the amount of remuneration paid to the employee for the eligibility period, on remuneration of up to $1,129 per week.  For non-arm’s length employees, the percentage would be applied to the employee’s weekly eligible remuneration<sup>8</sup> or pre-crisis remuneration<sup>9</sup>, whichever is less, up to a maximum of $1,129.  These limits on eligible remuneration apply to Periods 5 through 10.
 			</p>
 			<h4>Top-up Subsidy</h4>
 			<p>
-				<strong>General –</strong> Average monthly gross revenue for the last 3 calendar months that ended prior<sup>11</sup> to the current reference period is compared to the average of the same months in 2019.
+				<strong>General –</strong> Average monthly gross revenue for the last 3 calendar months that ended prior<sup>11</sup> to the current reference period is compared to the average of the same months in 2019. For periods 8,9 and 10 the following revenue decline method we be used instead of the three-month revenue-decline test if it is greater: the top-up would be determined by the change in an eligible employer's monthly revenues, year-over-year, for either the current or previous calendar month. See note 13.
 			</p>
 			<p>
-				<strong>Election –</strong> If the election is made for the base subsidy, the average monthly gross revenue for the last 3 calendar months that ended prior<sup>11</sup> to the current reference period is compared to the average monthly revenue in January and February 2020 for the top-up subsidy instead.
+				<strong>Election –</strong> If the election is made for the base subsidy, the average monthly gross revenue for the last 3 calendar months that ended prior<sup>11</sup> to the current reference period is compared to the average monthly revenue in January and February 2020 for the top-up subsidy instead. For periods 8,9 and 10 both the base subsidy and the top-up subsidy would be determined by the change in an eligible employer's current or prior monthly revenues relative to the average of its January 2020 and February 2020 revenues.
 			</p>
 			<p>
-				<strong>Top-up Subsidy Calculation –</strong> The 3-month average revenue decline in excess of 50% is multiplied by 1.25.  This new percentage, capped at 25%, is added to the percentage calculated under the base subsidy.
+				<strong>Top-up Subsidy Calculation –</strong> The revenue decline percentage in excess of 50% is multiplied by 1.25.  This new percentage, capped at 25%, is added to the percentage calculated under the base subsidy.
 			</p>
 			<h3>Period 10</h3>
 			<p>See note 13.</p>
--- src/content/GeneralNotesFooterNotes.tsx
+++ src/content/GeneralNotesFooterNotes.tsx
@@ -12,13 +12,13 @@ const GeneralNotesFooterNotes: React.FC<IProps> = (_props: IProps) => {
 					<p><strong>“Gross revenue” (qualifying revenue)</strong> is total revenues arising in the course of the ordinary activities of the eligible entity (generally from the sale of goods, the rendering of services and the use by others of resources of the eligible entity), not reduced by expenses. There are several restrictions and possible elections (some of which must be made for multiple or all periods) for determining gross revenue for a period.  For example, gross revenues could be calculated on the cash or accrual basis, non-arm’s length revenue is generally excluded (but there is an exception available), and there are possibilities for aggregating or separating income within corporate groups.  The calculation assumes that these items have been taken into account when the gross revenue is entered into the calculator for each particular month.</p>
 				</li>
 				<li id="general-notes-2">
-					<p><strong>“Current reference period”</strong> refers to a particular month in 2020 that is defined by legislation in respect of each particular claim period.  They are:  Period 1 – March, 2 – April, 3 – May, 4 – June, 5 – July, 6 – August, 7 – September, 8 – October, 9 – November.  Gross revenue from this month is used in determining entitlement to the subsidy.</p>
+					<p><strong>“Current reference period”</strong> refers to a particular month in 2020 that is defined by legislation in respect of each particular claim period.  They are:  Period 1 – March, 2 – April, 3 – May, 4 – June, 5 – July, 6 – August, 7 – September, 8 – October, 9 – November and 10 - December.  Gross revenue from this month is used in determining entitlement to the subsidy.</p>
 				</li>
 				<li id="general-notes-3">
 					<p><strong>“Current period”</strong> refers to the period for which the claim is being made.</p>
 				</li>
 				<li id="general-notes-4">
-					<p><strong>“Prior reference period”</strong> Gross revenue from the prior reference period is measured against that of the current reference period to determine the revenue reduction percentage5.  It is the same month from the prior year as the current reference period.  However, an election can be made which effectively changes it to January and February of 2020.  See “Alternative calculation” in note 6.</p>
+					<p><strong>“Prior reference period”</strong> Gross revenue from the prior reference period is measured against that of the current reference period to determine the revenue reduction percentage<sup>5</sup>.  It is the same month from the prior year as the current reference period.  However, an election can be made which effectively changes it to January and February of 2020.  See “Alternative calculation” in note 6.</p>
 				</li>
 				<li id="general-notes-5">
 					<p className="mb-0"><strong>“Revenue reduction percentage”</strong>of an eligible entity for a qualifying period is the percentage by which revenue has declined from the prior reference period to the current reference period.  In particular, it is calculated using this formula (expressed as a percentage): <strong>1 − A/B</strong><br /> where</p>
@@ -39,7 +39,7 @@ const GeneralNotesFooterNotes: React.FC<IProps> = (_props: IProps) => {
 				</li>
 				<li id="general-notes-8">
 					<p><strong>“Eligible remuneration”</strong> generally includes wages, salaries and benefits paid.  Note that the amount of remuneration eligible for the subsidy is limited by several factors.  It is assumed that the Estimator user has already determined the available weekly eligible remuneration (and baseline remuneration, where applicable) for each employee.</p>
-					<p className="mb-0">There are limits to the amount of eligible remuneration paid which is available for the subsidy.  See the “Subsidy Calculation” details in the General Estimator Logic section above.  There are different, but similar rules for Periods 1-4 as compared to Periods 5-9.  In the “Eligible Remuneration” column, please only enter the amount of eligible remuneration that is available as a result of the “Subsidy Calculation”.  Also, note that Eligible Remuneration paid is not calculated in respect of the reference month, but rather, in respect of the specific days covered by the 4-week claim period in 2020.  They claim periods are:</p>
+					<p className="mb-0">There are limits to the amount of eligible remuneration paid which is available for the subsidy.  See the “Subsidy Calculation” details in the General Estimator Logic section above.  There are different, but similar rules for Periods 1-4 as compared to Periods 5-10.  In the “Eligible Remuneration” column, please only enter the amount of eligible remuneration that is available as a result of the “Subsidy Calculation”.  Also, note that Eligible Remuneration paid is not calculated in respect of the reference month, but rather, in respect of the specific days covered by the 4-week claim period in 2020.  They claim periods are:</p>
 					<ul className="mb-3">
 						<li>Period 1 Mar 15 – April 11</li>
 						<li>Period 2 April 12 – May 9</li>
@@ -70,6 +70,7 @@ const GeneralNotesFooterNotes: React.FC<IProps> = (_props: IProps) => {
 						<li>Period 7 – June to August</li>
 						<li>Period 8 – July to September</li>
 						<li>Period 9 – August to October</li>
+						<li>Period 10 – September to November</li>
 					</ul>
 				</li>
 				<li id="general-notes-11">
@@ -90,6 +91,10 @@ const GeneralNotesFooterNotes: React.FC<IProps> = (_props: IProps) => {
 						| {` `}
 						<a href="https://www.canada.ca/en/department-finance/news/2020/07/adapting-the-canada-emergency-wage-subsidy-to-protect-jobs-and-promote-growth.html" target="_blank" rel="noreferrer noopener">
 							Backgrounder 2
+						</a> {` `}
+						| {` `}
+						<a href="https://www.canada.ca/en/department-finance/news/2020/10/extending-the-canada-emergency-wage-subsidy.html" target="_blank" rel="noreferrer noopener">
+							Backgrounder 3
 						</a> {` `}
 						| {` `}
 						<a href="https://www.canada.ca/en/revenue-agency/services/subsidy/emergency-wage-subsidy.html" target="_blank" rel="noreferrer noopener">
